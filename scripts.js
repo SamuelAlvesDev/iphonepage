7e4d15ea07b577175399529ba515bfeba63a717c
SamuelAlvesDev <105619015+SamuelAlvesDev@users.noreply.github.com>
2023-07-30 11:31:05 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function troca_Cor(cor){
+
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+function troca_Imagem(imagem){
+    let iphone = document.querySelector(".iphone_imagem")
+    iphone.src = imagem
+
+}
+
